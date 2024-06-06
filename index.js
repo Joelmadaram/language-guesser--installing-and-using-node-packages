7b5179cc63d07b57c91080b36ba15e6f0dacc023
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-06 16:46:07 -0500

    using if else to get the language name when given an input

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const colors = require('colors');
 
 const input = process.argv[2];
 const langCode = franc(input);
+if(langCode === 'und') {
+    console.log('Language not found'.red);
+}
+else{
+const language = langs.where('3', langCode);
+console.log(`our best guess is: ${language.name}`.green)
+}
