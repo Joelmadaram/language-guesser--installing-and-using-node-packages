9a2562f2cacb71330058f1f7a5318a7652d6796d
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-06 16:54:54 -0500

    adding package.json

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 const franc = require('franc');
 const langs = require('langs');
 const colors = require('colors');
-
-const input = process.argv[2];
-const langCode = franc(input);
-if(langCode === 'und') {
-    console.log('Language not found'.red);
-}
-else{
-const language = langs.where('3', langCode);
-console.log(`our best guess is: ${language.name}`.green)
-}
